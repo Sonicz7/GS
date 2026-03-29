aafeb4e07acbc309d175790973fecbb31fe6003c
Sonicz7 <sonicgf93@gmail.com>
2026-03-29 22:10:02 +0200

    Delete gs_out directory

--- gs_out/utils/rapportState.js
+++
@@ -1,7 +0,0 @@
-// Stockage en mémoire des sessions =rapport en cours
-// Clé : messageId de l'embed de suivi
-// Valeur : { rows, doneSet, doneMentions, totalMembers, channelId, guildId }
-
-const rapportSessions = new Map();
-
-module.exports = { rapportSessions };
