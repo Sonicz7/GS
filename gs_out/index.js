c1e6c74634a93195ada139b331797eff8ef3901a
Sonicz7 <sonicgf93@gmail.com>
2026-03-29 21:49:30 +0200

    Refactor index.js to remove HTTP server
    Removed HTTP server functionality and added keep_alive module.

--- gs_out/index.js
+++ gs_out/index.js
@@ -1,21 +1,6 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
-var http = require('http');
-
-let started = false;
-
-function start() {
-  if (started) return;
-  started = true;
-  http.createServer(function (req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.write("Gs est en ligne!");
-    res.end();
-  }).listen(process.env.PORT || 8080);
-}
-
-module.exports = start;
+const keep_alive = require('./keep_alive.js')
 const fs   = require('fs');
 const path = require('path');
 const { token } = require('./config');
