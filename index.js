38820952a51e56073b696ee63ea05044cea9f604
Sonicz7 <sonicgf93@gmail.com>
2026-05-10 12:12:02 +0200

    Import and invoke keepAlive function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const keepAlive = require('./keep_alive');
+keepAlive();
 const { Client, GatewayIntentBits, Collection, REST, Routes } = require('discord.js');
 const fs   = require('fs');
 const path = require('path');
